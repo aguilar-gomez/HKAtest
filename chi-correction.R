70326342cfe452f83a819322e48e8171c21b8c72
Diana Aguilar Gomez <diana@troutbuster.biol.berkeley.edu>
2020-09-07 15:11:23 -0700

    correction script

---
+++ chi-correction.R
@@ -0,0 +1,27 @@
+#!/usr/bin/env Rscript
+library(snpStats)
+
+##########Read Arguments###################################
+args = commandArgs(trailingOnly=TRUE)
+HKAtest<- read.delim(args[1])
+file<- args[2] #name of output
+
+##########Prepare Data###################################
+
+png(paste0("qqplot_chiHKA",file,".png"), width=600, height=600)
+qq.chisq(HKAtest$chi2,df=1)
+dev.off()
+qqresult<-qq.chisq(HKAtest$chi2,df=1)
+
+HKAtest$corrected<-HKAtest$chi2/qqresult["lambda"]
+
+png(paste0("qqplot_chiHKA_corrected",file,".png"), width=600, height=600)
+qq.chisq(HKAtest$corrected,df=1)
+dev.off()
+
+HKAtest$pval_corr<-pchisq(HKAtest$corrected, df=1, lower.tail=FALSE)
+HKAtest$pval2<-pchisq(HKAtest$chi2, df=1, lower.tail=FALSE)
+
+write.table(HKAtest,paste0(args[1],"_corrected.tab"),
+            quote = FALSE, row.names = FALSE, col.names = TRUE,
+            sep = "\t")
