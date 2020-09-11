80f915070597025367572056695966e7e03a731a
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2020-09-11 00:34:07 -0700

    Update chi-correction.R

--- chi-correction.R
+++ chi-correction.R
@@ -3,12 +3,12 @@ library(snpStats)
 
 ##########Read Arguments###################################
 args = commandArgs(trailingOnly=TRUE)
-HKAtest<- read.delim(args[1])
+HKAtest_raw<- read.delim(args[1])
 file<- args[2] #name of output
 min_sites<- args[3]
 
 ##########Prepare Data###################################
-HKAtest<-HKAtest[HKAtest$poly_gene+HKAtest$fix_gene>min_sites,]
+HKAtest<-HKAtest_raw[HKAtest_raw$poly_gene+HKAtest_raw$fix_gene>min_sites,]
 
 png(paste0("qqplot_chiHKA",file,".png"), width=600, height=600)
 qq.chisq(HKAtest$chi2,df=1)
