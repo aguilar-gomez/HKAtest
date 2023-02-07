2d9f2b30057b9175509997c1b0bcfa939b24981a
aguilar-gomez <35580465+aguilar-gomez@users.noreply.github.com>
2023-02-06 23:19:32 -0800

    Update HKAgenes.py

--- HKAgenes.py
+++ HKAgenes.py
@@ -7,20 +7,29 @@ import scipy.stats as ss
 import argparse
 import numpy as np
 
-def fixed(af1,af2):
+def fixed(af1,af2, minor1, minor2):
 	'''
 	Returns 1 if the allele is fixed in pop1, 0 otherwise
 	'''
 	af1_1 = (af1>fixed_af) & (af2<1-fixed_af)
 	af1_0 = (af1<1-fixed_af) & (af2>fixed_af)
 	af1_fixed = af1_1 | af1_0
-	return af1_fixed.astype(int)
+	
+	#Allele is not segregating in pop2 and the minor allele differs 
+	af2_fixed = (af2<1-fixed_af) | (af2>fixed_af)
+	triallelic= (!af1_fixed & af2_fixed) & (minor1 != minor2)
+		
+	fixed = triallelic | af1_fixed
+	
+	return fixed.astype(int)
 
-def poly(af):
+def poly(af1, af2):
 	'''
 	Returns 1 if the allele is polymorphic in pop1, 0 otherwise
 	'''
-	af_p = (af>=1-fixed_af) & (af<=fixed_af)
+	af1_p = (af1>=1-fixed_af) & (af1<=fixed_af) 
+	af2_fix = (af2<1-fixed_af) | (af2>fixed_af)
+	af_p = af1_p & af2_fix
 	return af_p.astype(int)
 
 def genome_wide(pop):
@@ -75,12 +84,12 @@ print("Analyzing "+str(len(p1p2))+" sites found in both populations")
 
 
 #Fixed D
-p1p2["pop1_fix"]=fixed(p1p2["knownEMpop1"],p1p2["knownEMpop2"])
-p1p2["pop2_fix"]=fixed(p1p2["knownEMpop2"],p1p2["knownEMpop1"])
+p1p2["pop1_fix"]=fixed(p1p2["knownEMpop1"],p1p2["knownEMpop2"],p1p2["minorpop1"],p1p2["minorpop2"])
+p1p2["pop2_fix"]=fixed(p1p2["knownEMpop2"],p1p2["knownEMpop1"],p1p2["minorpop2"],p1p2["minorpop1"])
 
 #Segregating/polymorphic P
-p1p2["pop1_seg"]=poly(p1p2["knownEMpop1"])
-p1p2["pop2_seg"]=poly(p1p2["knownEMpop2"])
+p1p2["pop1_seg"]=poly(p1p2["knownEMpop1"],p1p2["knownEMpop2"])
+p1p2["pop2_seg"]=poly(p1p2["knownEMpop2"],p1p2["knownEMpop1"])
 
 #Genome wide proportions
 p1_pol_prop, p1_fix_prop = genome_wide("pop1")
